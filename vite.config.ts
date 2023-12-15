12734a0211e76a86d58fe4040b638c582d34d305
Andres Morey <andresmarcel@gmail.com>
2023-12-15 14:29:55 +0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { babelOptimizerPlugin } from '@graphql-codegen/client-preset';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    splitVendorChunkPlugin()
+    react({
+      babel: {
+        presets: [
+          'react-app',
+        ],
+        plugins: [
+          [babelOptimizerPlugin, { artifactDirectory: './src/gql', gqlTagName: 'graphql' }],
+        ],  
+      }
+    }),
+    splitVendorChunkPlugin(),
   ],
 })
